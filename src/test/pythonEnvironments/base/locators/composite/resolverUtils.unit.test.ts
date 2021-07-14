7bdd573644750fa6a37498318a74a1f566c4a835
Kartik Raj <karraj@microsoft.com>
2021-07-14 12:20:41 -0700

    Only attempt to resolve using registry if source is set as WindowsRegistry

--- src/test/pythonEnvironments/base/locators/composite/resolverUtils.unit.test.ts
+++ src/test/pythonEnvironments/base/locators/composite/resolverUtils.unit.test.ts
@@ -32,8 +32,6 @@ suite('Resolver Utils', () => {
         const testPyenvVersionsDir = path.join(testPyenvRoot, 'versions');
         setup(() => {
             sinon.stub(externalDependencies, 'getWorkspaceFolders').returns([]);
-            sinon.stub(winreg, 'readRegistryValues').resolves([]);
-            sinon.stub(winreg, 'readRegistryKeys').resolves([]);
             sinon.stub(platformApis, 'getEnvironmentVariable').withArgs('PYENV_ROOT').returns(testPyenvRoot);
         });
 
@@ -72,8 +70,6 @@ suite('Resolver Utils', () => {
 
         setup(() => {
             sinon.stub(externalDependencies, 'getWorkspaceFolders').returns([]);
-            sinon.stub(winreg, 'readRegistryValues').resolves([]);
-            sinon.stub(winreg, 'readRegistryKeys').resolves([]);
             sinon.stub(platformApis, 'getEnvironmentVariable').withArgs('LOCALAPPDATA').returns(testLocalAppData);
         });
 
@@ -206,8 +202,6 @@ suite('Resolver Utils', () => {
 
         setup(() => {
             sinon.stub(externalDependencies, 'getWorkspaceFolders').returns([]);
-            sinon.stub(winreg, 'readRegistryValues').resolves([]);
-            sinon.stub(winreg, 'readRegistryKeys').resolves([]);
         });
 
         teardown(() => {
@@ -273,8 +267,6 @@ suite('Resolver Utils', () => {
         const testVirtualHomeDir = path.join(TEST_LAYOUT_ROOT, 'virtualhome');
         setup(() => {
             sinon.stub(externalDependencies, 'getWorkspaceFolders').returns([testVirtualHomeDir]);
-            sinon.stub(winreg, 'readRegistryValues').resolves([]);
-            sinon.stub(winreg, 'readRegistryKeys').resolves([]);
         });
 
         teardown(() => {
@@ -487,6 +479,7 @@ suite('Resolver Utils', () => {
             const actual = await resolveEnvUsingKind({
                 executablePath: interpreterPath,
                 kind: PythonEnvKind.Unknown,
+                source: [PythonEnvSource.WindowsRegistry],
             });
             const expected = buildEnvInfo({
                 location: path.join(regTestRoot, 'py39'),
@@ -507,6 +500,7 @@ suite('Resolver Utils', () => {
             const actual = await resolveEnvUsingKind({
                 executablePath: interpreterPath,
                 kind: PythonEnvKind.Unknown,
+                source: [PythonEnvSource.WindowsRegistry],
             });
             const expected = buildEnvInfo({
                 location: path.join(regTestRoot, 'python38'),
@@ -530,6 +524,7 @@ suite('Resolver Utils', () => {
             const actual = await resolveEnvUsingKind({
                 executablePath: interpreterPath,
                 kind: PythonEnvKind.Conda,
+                source: [PythonEnvSource.WindowsRegistry],
             });
             const expected = buildEnvInfo({
                 location: path.join(regTestRoot, 'conda3'),
